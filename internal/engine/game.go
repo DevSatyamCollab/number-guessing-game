7918a8a063f7a3eeca0ab6613dbf99b5e87c99f5
DevSatyamCollab <satyamkanungo0@gmail.com>
2026-02-20 19:56:17 +0530

    feat: Implement GameEngine and result state logic

--- internal/engine/game.go
+++ internal/engine/game.go
@@ -1,1 +1,45 @@
+package engine
 
+import "math/rand"
+
+type IGame interface {
+	Check(guess int) GuessResult
+	GenerateRandomNumber() int
+}
+
+type GuessResult int
+
+const (
+	High GuessResult = iota
+	Low
+	Correct
+)
+
+type GameEngine struct {
+	MaxAttempts int
+	MysteryNum  int
+}
+
+func NewGame(attmepts int) *GameEngine {
+	return &GameEngine{
+		MysteryNum:  generateMysteryNumber(),
+		MaxAttempts: attmepts,
+	}
+}
+
+func (g *GameEngine) Check(guess int) GuessResult {
+	if g.MysteryNum == guess {
+		return Correct
+	}
+
+	if g.MysteryNum < guess {
+		return Low
+	}
+
+	return High
+}
+
+func generateMysteryNumber() int {
+	max, min := 101, 1
+	return rand.Intn(max-min) + min
+}
